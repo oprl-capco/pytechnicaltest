92a4c22ead194e581466f961f96a0cc071a153bd
Oliver <oliver.pressland@capco.com>
2021-10-07 09:23:43 +0100

    Changed exercise for PYT4 as linspace array execise might be too difficult.

---
+++ problems/pyt4_caseconverter.py
@@ -0,0 +1,38 @@
+# PYT4 - Case Converter
+
+# An application is specified so that a given string can
+# be converted from "snake_case" to "CamelCase" and vice versa.
+
+# For example:
+# from "this_method"
+# to "ThisMethod"
+
+# Or from "ThisMethod"
+# to "this_method"
+
+# The client can specify in the query string what method of conversion they want
+# and also what word they would like to convert.
+
+# Using this information, complete the "to_camel" and the "to_snake" function.
+
+# Your code must pass the unit tests in tests/test_pyt4_caseconverter.py
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Write your code below this line
+
+
+@app.get("/exercise/camel/{word}")
+async def to_camel(word: str):
+    pass
+
+
+@app.get("/exercise/snake/{word}")
+async def to_snake(word: str):
+    pass
+
+    
+
---
+++ problems/tests/test_pyt4_caseconverter.py
@@ -0,0 +1,22 @@
+# Your answers in pyt4_caseconverter.py must pass these tests
+# Do not modify the unit tests
+
+from fastapi.testclient import TestClient
+
+from problems.pyt4_caseconverter import app
+
+client = TestClient(app)
+
+
+class TestClass4:
+
+    def test_should_be_camel(self):
+        response = client.get("/exercise/camel/this_method")
+        assert response.status_code == 200
+        assert response.json() == "ThisMethod"
+
+    def test_should_be_snake(self):
+        response = client.get("/exercise/snake/ThisMethod")
+        assert response.status_code == 200
+        assert response.json() == "this_method"
+
